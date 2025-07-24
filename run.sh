80a83d7743f6d5c31ae386b5a603340f13b9857b
shaojun0 <2211057934@qq.com>
2025-07-24 11:47:24 +0800

    deepspeed 2模型并行2数据并行增加

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ apt update
 apt install openmpi-bin openmpi-common libopenmpi-dev
 pip install nv
 uv sync
-uv run train.py
+source .venv/bin/activate
+deepspeed --num_gpus=2 train.py
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from transformers import TrainingArguments, Qwen2_5_VLForConditionalGeneration, 
 
 from collector import qwen_2_5_collator
 from dataset import ImageOnlyDecoderCaptioningDataset
+import json
 
 
 def run_only_decoder(is_scnet=False):
@@ -44,13 +45,16 @@ def run_only_decoder_deepspeed(is_scnet=False):
     train_dataset = load_dataset(dataset_files,split="train[:1%]")
     eval_dataset = load_dataset(dataset_files,split="train[1%:2%]")
     output_dir = "outputs"
-    deep_speed_path = "DeepSpeedExamples/training/autotuning/hf/dsconfigs/ds_config_fp16_z3.json"
+    deep_speed_path = "DeepSpeedExamples/training/autotuning/hf/dsconfigs/ds_config_z0.json"
+    with open(deep_speed_path,encoding="utf-8") as f:
+        deep_speed_config = json.load(f)
+    deep_speed_config["sequence_parallel_size"]=2
     training_args = TrainingArguments(output_dir=output_dir,
                                       per_device_train_batch_size=2,
                                       per_device_eval_batch_size=8,
                                       num_train_epochs=3,
                                       save_safetensors=True,
-                                      deepspeed=deep_speed_path,
+                                      deepspeed=deep_speed_config,
                                       fp16=True)
     model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_path)
     processor = Qwen2_5_VLProcessor.from_pretrained(model_path)
