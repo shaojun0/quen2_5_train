ce64b4e3f18eeff4d19a04541bbb2f72dfdffb26
shaojun0 <2211057934@qq.com>
2025-07-24 12:00:32 +0800

    deepspeed 2模型并行2数据并行增加

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@ apt update
 apt install openmpi-bin openmpi-common libopenmpi-dev
 pip install nv
 uv sync
-source .venv/bin/activate
-deepspeed --num_gpus=2 train.py
+uvx deepspeed --num_gpus=2 train.py
